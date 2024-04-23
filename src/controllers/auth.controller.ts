e7ddd003b73555a4b1fdb70f06aa49cf662741a9
Angel Velez <angelvelez1415@gmail.com>
2024-04-23 09:56:08 -0500

    ok

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,32 @@
+const bcrypt = require("bcryptjs");
+const db = require('../config/config-db.js');
+import UserRepository from "../repositories/User/userRepository";
+import { Request, Response } from "express";
+
+let auth = async (req: Request, res: Response) => {
+      try {
+        const {
+            email, 
+            password
+        } = req.body;
+        
+        const result: any = await UserRepository.auth(email);
+
+        if (result[0].length > 0){
+          const isPasswordValid = await bcrypt.compare(password, result[0][0].password);
+          if (isPasswordValid){
+            return res.status(200).json({ 
+              status: 'Successful authentication'
+            });
+          }
+        }
+        return res.status(401).json({ 
+          status: 'Incorrect username or password'
+        });
+      } catch (error) {
+        console.log(error);
+      }
+}
+
+
+export default auth;
---
+++ src/repositories/User/userRepository.ts
@@ -0,0 +1,20 @@
+import db from '../../config/config-db';
+import User from '../../Dto/UserDto';
+
+class UserRepository {
+
+    static async add(user: User){
+        const sql = 'INSERT INTO usuarios (email, nombres, apellidos, contrasenia, telefono) VALUES (?, ?, ?, ?, ?)';
+        const values = [user.email, user.nombres, user.apellidos, user.password, user.telefono];
+        
+    }
+
+    static async auth(email: string){
+        const sql = 'SELECT contrasenia FROM usuarios WHERE email = ?';
+        const values = [email];
+        return db.execute(sql, values);
+    }
+
+}
+
+export default UserRepository;
